4d07c797596770d59def39f90dc14d01bf9e4ed0
Lee suyoun <20210370@gmail.com>
2024-08-10 06:51:55 +0900

    Changes

--- src/main/java/com/artistry/artistry/service/CollaborationChatService.java
+++ src/main/java/com/artistry/artistry/service/CollaborationChatService.java
@@ -4,6 +4,7 @@ import com.artistry.artistry.entity.CollaborationChat;
 import com.artistry.artistry.repository.CollaborationChatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
 import java.util.List;
 
--- src/main/java/com/artistry/artistry/service/SecondHandChatService.java
+++ src/main/java/com/artistry/artistry/service/SecondHandChatService.java
@@ -4,6 +4,7 @@ import com.artistry.artistry.entity.SecondHandChat;
 import com.artistry.artistry.repository.SecondHandChatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
 import java.util.List;
 
